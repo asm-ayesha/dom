eccd8d1292c35c471db41ba8892f0c9531002eed
asm-ayesha <ayesha219333@gmail.com>
2026-01-02 16:07:39 +0600

    styling dom properties

--- js/dom.js
+++ js/dom.js
@@ -7,5 +7,13 @@ for(let li of liCollection){
 const headingCollection = document.getElementsByTagName('h1');
 
 for(let h1 of headingCollection){
-    console.log(h1.innerText)
+    console.log(h1.style.color='red')
+}
+
+const sections = document.querySelectorAll('section')
+for(let section of sections){
+    section.style.border = '2px solid steelblue';
+    section.style.marginBottom = '5px';
+    section.style.borderRadius = '5px';
+    section.style.backgroundColor = 'lightgray';
 }
