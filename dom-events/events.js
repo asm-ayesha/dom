568d88db92ad662de71fb008643ed15199fff29d
asm-ayesha <ayesha219333@gmail.com>
2026-02-15 11:45:08 +0600

    addEventLisiner on a button

--- dom-events/events.js
+++ dom-events/events.js
@@ -9,3 +9,14 @@ const btnMakeBlue = document.getElementById('btn-make-blue');
 btnMakeBlue.onclick = function makeBlue(){
     document.body.style.backgroundColor = 'blue';
 }
+
+
+
+document.getElementById('btn-make-red').addEventListener('click', function(){
+    document.body.style.backgroundColor = 'red';
+})
+
+
+document.getElementById('btn-make-gold').addEventListener('click', function(){
+    document.body.style.backgroundColor = 'gold';
+})
