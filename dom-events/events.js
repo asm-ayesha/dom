986af73d387cdf67d015351a0bc7060ca2f97807
asm-ayesha <ayesha219333@gmail.com>
2026-02-15 11:31:32 +0600

    add onclick handler directly

---
+++ dom-events/events.js
@@ -0,0 +1,11 @@
+function makeYellow(){
+    document.body.style.backgroundColor = 'yellow';
+}
+function makeGreen(){
+    document.body.style.backgroundColor = 'green';
+}
+
+const btnMakeBlue = document.getElementById('btn-make-blue');
+btnMakeBlue.onclick = function makeBlue(){
+    document.body.style.backgroundColor = 'blue';
+}
