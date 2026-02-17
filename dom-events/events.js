adc0c78e513fc753eacc632cb3a31b0d3a4050d8
asm-ayesha <ayesha219333@gmail.com>
2026-02-17 18:22:08 +0600

    event bubble and sttop propagration

--- dom-events/events.js
+++ dom-events/events.js
@@ -1,22 +1,43 @@
-function makeYellow(){
-    document.body.style.backgroundColor = 'yellow';
-}
-function makeGreen(){
-    document.body.style.backgroundColor = 'green';
-}
+// function makeYellow() {
+//     document.body.style.backgroundColor = 'yellow';
+// }
+// function makeGreen() {
+//     document.body.style.backgroundColor = 'green';
+// }
 
-const btnMakeBlue = document.getElementById('btn-make-blue');
-btnMakeBlue.onclick = function makeBlue(){
-    document.body.style.backgroundColor = 'blue';
-}
+// const btnMakeBlue = document.getElementById('btn-make-blue');
+// btnMakeBlue.onclick = function makeBlue() {
+//     document.body.style.backgroundColor = 'blue';
+// }
 
 
 
-document.getElementById('btn-make-red').addEventListener('click', function(){
-    document.body.style.backgroundColor = 'red';
+// document.getElementById('btn-make-red').addEventListener('click', function () {
+//     document.body.style.backgroundColor = 'red';
+// })
+
+
+// document.getElementById('btn-make-gold').addEventListener('click', function () {
+//     document.body.style.backgroundColor = 'gold';
+// })
+
+
+
+
+
+document.getElementById('btn-update-title').addEventListener('click', function(){
+    console.log("gyuyf")
+    const pageTitleEl = document.getElementById('page-title');
+    pageTitleEl.innerText = 'upadte this text'
 })
 
 
-document.getElementById('btn-make-gold').addEventListener('click', function(){
-    document.body.style.backgroundColor = 'gold';
+
+document.getElementById('btn-update').addEventListener('click', function(){
+    const nameInput = document.getElementById('name-input');
+    const name = nameInput.value ;
+    console.log(name)
+    const nameP = document.getElementById('name');
+    nameP.innerText = name;
 })
+
